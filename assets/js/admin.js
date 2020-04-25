64b73ec4a3eda88afbe6c5c430ab7a0f6d5de044
Gert Hammes <gert.hammes@brain-appeal.com>
2020-04-25 20:06:29 +0200

    [FEATURE] only allow selecting services for selected service systems in implementation project admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,26 +1,43 @@
-/*
- * Welcome to your app's main JavaScript file!
+/**
+ * This file is part of the KDN OZG package.
  *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * @author    Gert Hammes <info@gerthammes.de>
+ * @copyright 2020 Gert Hammes
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
-
 // replacement of require("@babel/polyfill");
 require( ["core-js/stable", 'regenerator-runtime/runtime']);
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/admin.scss');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-
-jQuery(document).ready(function() {
-    let $chartContainers = $('.mb-chart-container');
-    if ($chartContainers.length > 0) {
-        import('./modules/chart').then(({ default: baChart }) => {
-            baChart.setUpList($chartContainers);
+// jQuery is included globally outside of webpack!
+import $ from 'jquery';
+//global.$ = $;
+const appOnReady = function() {
+    let chartContainers = document.querySelectorAll('.mb-chart-container');
+    if (chartContainers.length > 0) {
+        import('./modules/chart').then(({ default: appChart }) => {
+            appChart.setUpList(chartContainers);
 
         }).catch(error => 'An error occurred while loading the chart component');
     }
-});
+    let advancedSelectElements = document.querySelectorAll('select.js-advanced-select');
+    if (advancedSelectElements.length > 0) {
+        import('./modules/advanced-select').then(({ default: appAdvanceSelect }) => {
+            appAdvanceSelect.setUpList(advancedSelectElements);
+
+        }).catch(error => 'An error occurred while loading the chart component');
+    }
+};
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    appOnReady();
+} else {
+    document.addEventListener("DOMContentLoaded", appOnReady);
+}
--- assets/js/modules/chart.js
+++ assets/js/modules/chart.js
@@ -1,31 +1,39 @@
+/**
+ * This file is part of the KDN OZG package.
+ *
+ * @author    Gert Hammes <info@gerthammes.de>
+ * @copyright 2020 Gert Hammes
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['jquery', 'chart.js'], function ($, Chart) {
-            return factory($, Chart);
+        define(['chart.js'], function (Chart) {
+            return factory(Chart);
         });
     } else if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        module.exports = factory(require('jquery', 'chart.js'));
+        module.exports = factory(require('chart.js'));
     } else {
         // Browser globals (root is window)
         // noinspection JSUndefinedPropertyAssignment
-        root.brainChart = factory(root.jquery, root.Chart);
+        root.appChart = factory(root.Chart);
     }
-}(this, function ($, Chart) {
+}(this, function (Chart) {
     "use strict";
     return {
-        setUpList: function ($chartContainers) {
+        setUpList: function (chartContainers) {
             let self = this;
-            $chartContainers.each(function () {
-                self.load($(this));
-            });
+            for (let i = 0; i < chartContainers.length; i++) {
+                self.load(chartContainers.item(i));
+            }
         },
 
         parseFunction: function (str) {
-            let self = this;
             let fn_body_idx = str.indexOf('{'),
                 fn_body = str.substring(fn_body_idx + 1, str.lastIndexOf('}')),
                 fn_declare = str.substring(0, fn_body_idx),
@@ -58,34 +66,38 @@
             return rawData;
         },
 
-        initializeCanvas: function ($chartContainer, chartConfig) {
+        initializeCanvas: function (container, chartConfig) {
             let self = this;
-            let $canvas = $('<canvas class="mb-statistics-chart"></canvas>');
-            $chartContainer.html($canvas);
+            let canvas = document.createElement("canvas");
+            canvas.setAttribute("class", "mb-statistics-chart");
+            container.innerHTML = '';
+            container.appendChild(canvas);
             const data = self.parseJson(chartConfig);
-            const context = $canvas.get(0).getContext('2d');
+            const context = canvas.getContext('2d');
             let myChart = new Chart(context, data);
         },
 
-        load: function($chartContainer, url) {
+        load: function(container) {
             let self = this;
             if (self.chart) {
                 self.chart.destroy();
             }
-            let container = $chartContainer.get(0);
             self.addLoader(container);
-            $.ajax({
-                type: "GET",
-                url: container.getAttribute('data-url')
-            }).done(function(data){
-                if (data && data.chartConfig) {
-                    self.initializeCanvas($chartContainer, data.chartConfig);
-                } else {
-                    container.innerHTML = '<div class="message-chart-empty">No data found</div>';
-                }
-            }).fail(function(data){
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
                 container.innerHTML = '';
-            });
+                if (this.readyState === 4 && this.status === 200) {
+                    const data = JSON.parse(this.responseText);
+                    if (data && data.chartConfig) {
+                        self.initializeCanvas(container, data.chartConfig);
+                    } else {
+                        container.innerHTML = '<div class="message-chart-empty">No data found</div>';
+                    }
+                }
+            };
+
+            xhttp.open("GET", container.getAttribute('data-url'), true);
+            xhttp.send();
         },
         addLoader: function(container) {
             container.innerHTML = '<div class="chart-loader"><i class="fa fa-cog fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span></div>';
