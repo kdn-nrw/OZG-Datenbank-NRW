13d131bb02942a2d14a1f4e7306c0bca0a8f77d3
Gert Hammes <gert.hammes@brain-appeal.com>
2020-05-22 14:39:50 +0200

    [TASK] change form server solutions form type depending on number of entities to preven out of memory error

--- src/Entity/FormServer.php
+++ src/Entity/FormServer.php
@@ -31,7 +31,7 @@ class FormServer extends BaseNamedEntity
     /**
      * @var FormServerSolution[]|Collection
      * @ORM\OneToMany(targetEntity="FormServerSolution", mappedBy="formServer", cascade={"all"}, orphanRemoval=true)
-     * @ORM\OrderBy({"position" = "ASC", "name" = "ASC"})
+     * @ORM\OrderBy({"position" = "ASC", "id" = "ASC"})
      */
     private $formServerSolutions;
 
