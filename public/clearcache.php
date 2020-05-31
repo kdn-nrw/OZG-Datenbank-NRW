a5dbda79bd0fa279b668aeebe3d81ab3342fa35c
Gert Hammes <gert.hammes@brain-appeal.com>
2020-05-31 21:54:52 +0200

    [TASK] update backend profile dropdown

---
+++ public/clearcache.php
@@ -0,0 +1,45 @@
+<?php
+$dir = '/home/kdn/webspace/domain/ozg.kdn.de/var/cache/dev';
+if (is_dir($dir)) {
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::CHILD_FIRST
+    );
+
+    foreach ($files as $fileinfo) {
+        $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+        $todo($fileinfo->getRealPath());
+    }
+
+    rmdir($dir);
+}
+
+$dir = '/home/kdn/webspace/domain/ozg.kdn.de/var/cache/prod';
+if (is_dir($dir)) {
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::CHILD_FIRST
+    );
+
+    foreach ($files as $fileinfo) {
+        $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+        $todo($fileinfo->getRealPath());
+    }
+
+    rmdir($dir);
+}
+
+$dir = '/home/kdn/webspace/domain/ozg.kdn.de/_backup';
+if (is_dir($dir)) {
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::CHILD_FIRST
+    );
+
+    foreach ($files as $fileinfo) {
+        $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+        $todo($fileinfo->getRealPath());
+    }
+
+    rmdir($dir);
+}
