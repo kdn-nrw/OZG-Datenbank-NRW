e87199e28d70b278b4122a0151f307e0b44122b7
Gert Hammes <gert.hammes@brain-appeal.com>
2021-09-16 14:01:36 +0200

    [BUGFIX] fix frontend ajax calls to commune solutions and integrations

--- src/Controller/CommuneController.php
+++ src/Controller/CommuneController.php
@@ -13,6 +13,13 @@ namespace App\Controller;
 
 
 use App\Admin\Frontend\CommuneAdmin;
+use App\DependencyInjection\InjectionTraits\InjectManagerRegistryTrait;
+use App\Entity\Base\BaseEntityInterface;
+use App\Entity\StateGroup\Commune;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * Class CommuneController
@@ -23,6 +30,8 @@ use App\Admin\Frontend\CommuneAdmin;
  */
 class CommuneController extends AbstractFrontendCRUDController
 {
+    use InjectManagerRegistryTrait;
+
     /**
      * @inheritDoc
      */
@@ -37,5 +46,70 @@ class CommuneController extends AbstractFrontendCRUDController
     protected function getAdminClassName(): string
     {
         return CommuneAdmin::class;
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function integrationsAction(Request $request, int $id): Response
+    {
+        $commune = $this->getEntityManager()->find(Commune::class, $id);
+        if (null === $commune || !$this->admin->isGranted('SHOW')) {
+            $this->createAccessDeniedException();
+        }
+        return $this->renderPartialTemplate(
+            $commune,
+            'communeSolutions',
+            'CommuneAdmin/show-commune-solution-integration.html.twig'
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function solutionsAction(Request $request, int $id): Response
+    {
+        $commune = $this->getEntityManager()->find(Commune::class, $id);
+        if (null === $commune || !$this->admin->isGranted('SHOW')) {
+            $this->createAccessDeniedException();
+        }
+        return $this->renderPartialTemplate(
+            $commune,
+            'solutions',
+            'CommuneAdmin/show-commune-solutions.html.twig'
+        );
+    }
+
+    /**
+     * @param BaseEntityInterface $object
+     * @param string $property
+     * @param string $view
+     * @return Response
+     */
+    protected function renderPartialTemplate(BaseEntityInterface $object, string $property, string $view): Response
+    {
+        $show = $this->admin->getShow();
+        if (null === $show) {
+            $this->createAccessDeniedException();
+        }
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+        $fieldDescription = $show->get($property);
+        $parameters = [
+            'entities' => $propertyAccessor->getValue($object, $property),
+            'object' => $object,
+            'field_description' => $fieldDescription,
+        ];
+
+        $response = $this->renderWithExtraParams($view, $parameters);
+        $jsonData = [
+            'type' => 'content',
+            'status' => 200,
+            'content' => $response->getContent(),
+        ];
+        return new JsonResponse($jsonData);
     }
 }
