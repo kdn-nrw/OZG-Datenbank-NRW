513d7228eda2c71a4b2ba34a33a3cca4a62c5db9
Gert Hammes <info@gerthammes.de>
2022-11-13 16:48:39 +0100

    [FEATURE] add commune property for portal interface url

--- src/Migrations/Version20221113153246.php
+++ src/Migrations/Version20221113153246.php
@@ -20,7 +20,7 @@ final class Version20221113153246 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        if ($schema->getTable('ozg_solutions_communes')->hasColumn('solution_ready')) {
+        if (!$schema->getTable('ozg_solutions_communes')->hasColumn('solution_ready')) {
             $this->addSql('ALTER TABLE ozg_solutions_communes DROP solution_ready, DROP solution_ready_at');
         }
     }
@@ -28,7 +28,7 @@ final class Version20221113153246 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        if (!$schema->getTable('ozg_solutions_communes')->hasColumn('solution_ready')) {
+        if ($schema->getTable('ozg_solutions_communes')->hasColumn('solution_ready')) {
             $this->addSql('ALTER TABLE ozg_solutions_communes ADD solution_ready TINYINT(1) NOT NULL, ADD solution_ready_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime)\'');
         }
     }
